2877b014fdc3ce5a7f611fdc1255fd07dccfae90
beav <39977088+andy-beaver@users.noreply.github.com>
2025-02-21 12:47:42 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://andy-beaver.github.io/new-onsite-dashboard',
+  site: 'https://andy-beaver.github.io',
   base: '/',
   integrations: [tailwind(), react()],
   output: 'static'
